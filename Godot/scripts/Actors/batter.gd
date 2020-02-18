bf9a66c1dcd418386031891d897d1d76725d85bb
Toby Glaze <toby.r.glaze@gmail.com>
2020-02-17 23:25:58 -0800

    animated batter sprite

--- Godot/scripts/Actors/batter.gd
+++ Godot/scripts/Actors/batter.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
-class_name Batter
-onready var anim = get_node("bat_idle_anim")
+onready var anim = get_node("AnimatedSprite")
 var velocity: = Vector2.ZERO # start with no velocity
 var speed: = 200
 var batterPositionRange: = 400
@@ -18,8 +17,8 @@ func _physics_process(delta): # delta times things with clock/render cycle
 	
 	velocity = move_and_slide(velocity)  # delta auto in move and slide function
 	
-	if(Input.get_action_strength("swing_bat")):
-		anim.play("Swing")
+	if(Input.is_action_pressed("swing_bat")):
+		anim.play("swing")
+	else:
+		anim.play("idle")
 	
-	if(not anim.is_playing()):
-		anim.play("bat_swing")
