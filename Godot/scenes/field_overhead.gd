f43905a3d3db1275003cc9ff1f3c246da38263d0
Toby Glaze <toby.r.glaze@gmail.com>
2020-02-29 10:18:22 -0800

    hit speed test

--- Godot/scenes/field_overhead.gd
+++ Godot/scenes/field_overhead.gd
@@ -1,15 +1,27 @@
 extends Node2D
 onready var anim = get_node("anim_ball_in_play/ball_in_play")
+onready var console = get_node("anim_ball_in_play/test_label")
+
 #IP(In Play) ["1", "P"], ["2", "C"],["3", "1B"],["4", "2B"],["5", "3B"],["6", "SS"],["7", "LF"],["8", "CF"],["9", "RF"]]
 var ball_status = "IP" 
 
 func _process(delta):
 	if Input.is_action_just_released("toggle_menu") == true:
 		get_tree().change_scene("res://scenes/mainMenu.tscn")
-		
+	
+	
+	
 	if ball_status == "IP":
+		
+		# randomize animations based on accuracy of swing
+		anim.playback_speed = hit_speed()
+		
 		anim.play("line_drive")
-		ball_status = "P"
+		ball_status = "P" # resetting ball position to pitcher after line drive (will be changed)
 	else:
 		pass
 
+func hit_speed():
+	var random_speed = randf()*4+1 # random speed between 1 and 3
+	console.text = str(random_speed)
+	return random_speed
