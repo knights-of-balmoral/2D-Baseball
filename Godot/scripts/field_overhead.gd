f8a78b90e35538b28f6a1ccf465f11f6daf1dd22
Toby Glaze <toby.r.glaze@gmail.com>
2020-06-05 14:00:20 -0700

    more hit work

--- Godot/scripts/field_overhead.gd
+++ Godot/scripts/field_overhead.gd
@@ -1,48 +1,49 @@
 extends Node2D
 onready var anim = get_node("anim_ball_in_play/anim_start_point/ball_in_play")
-onready var sp = get_node("anim_ball_in_play/anim_start_point")
+onready var ball_pos = get_node("anim_ball_in_play/anim_start_point")
 onready var ball_shadow = get_node("anim_ball_in_play/anim_start_point/ball_shadow")
+onready var lbl_ball_pos = get_node("anim_ball_in_play/anim_start_point/coords") # default ball start = 540,520
+
 
 func _ready():
-	pass
+	# get ball direction
+	globals.ball_x = calcBallX()
+	globals.ball_y = calcBallY()
+	# get ball velocity
+	globals.ball_speed = calcBallVelocity()
+	# get ball endpoint
+	##
 	
+	# If the ball is hit
+	if globals.ball_status == "IP":
+		globals.ball_status = "AIR"
+		anim.playback_speed = globals.ball_speed
+		anim.play("line_drive")
+
 func _process(delta):
-	
-	# See if players presses menu key
+	lbl_ball_pos.text = "Pos:" + str(ball_pos.position)
+	# See if player presses menu key
 	if Input.is_action_just_released("toggle_menu") == true:
 		get_tree().change_scene("res://scenes/mainMenu.tscn")
 		globals.ball_status = "P"
 	
-	# If the ball is hit
-	if globals.ball_status == "IP":
-		globals.ball_x = calculateBallX()
-		globals.ball_y = calculateBallY()
-		globals.ball_status = "AIR"
-		# default ball start = 540,500
-		# get ball velocity
-		# get ball direction
-		# get ball endpoint
+	if globals.ball_status == "AIR":
+		# transform the position of the animation
+		ball_pos.position.y -= globals.ball_y
+		ball_pos.position.x += globals.ball_x 
+	
+	if anim.current_animation_position >= 1.9:
+			ball_shadow.position.x = ball_pos.position.x
+			ball_shadow.position.y = ball_pos.position.y
+			globals.ball_status = "P" #"END"
+
 		
-	if globals.ball_status == "AIR":	
-		anim.playback_speed = 0.75
-		anim.play("line_drive")
-		
-		# transform from the starting point of the animation
-		sp.position.y -= globals.ball_y
-		sp.position.x += globals.ball_x 
-				
-		
-		if anim.current_animation_position >= 1.9:
-			globals.ball_status = "END"
-			ball_shadow.position.x = sp.position.x
-			ball_shadow.position.y = sp.position.y
-		
-func calculateBallY():
+func calcBallY():
 	randomize()
 	var num = randf()*1 + 1 # Y will most likely be always subtracted unless it's a foul ball straight back
-	return num + globals.ball_speed
+	return num
 	
-func calculateBallX():
+func calcBallX():
 	randomize()
 	var num = randf()*1 + 1 
 	
@@ -58,6 +59,11 @@ func calculateBallX():
 	
 	return num 
 
+func calcBallVelocity():
+	randomize()
+	var num = randf()*1 + 1
+	return num
+	
 func hit_animation():
 	var anim_matrix = ["line_drive","line_drive", "pop_fly", "foul_ball"] 
 	var rand = randi()%3 + 1
