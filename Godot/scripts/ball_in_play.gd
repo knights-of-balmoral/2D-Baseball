5ae4d9c7a64fa9c99317bbdc66b89bc807a41b1b
Stryyder <toby.r.glaze@gmail.com>
2020-10-17 14:04:48 -0700

    saturday update

--- Godot/scripts/ball_in_play.gd
+++ Godot/scripts/ball_in_play.gd
@@ -10,16 +10,24 @@ onready var fielder_7 = get_node("../../defense/fielder_7")
 onready var fielder_8 = get_node("../../defense/fielder_8")
 onready var fielder_9 = get_node("../../defense/fielder_9")
 var ball_thrown = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+# default throw velocity
+const THROW_VELOCITY = 10 # later to be multiplied by normal of vector direction we want ot throw in
+
+# distance per second ball will move
+var velocity = Vector2.ZERO
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(false)
 	
 func _physics_process(delta):
+	velocity.y += 1 * delta
+	var collision = move_and_collide(velocity * delta)
+	if collision != null:
+		_on_impact(collision.normal)
+	
+	
 	if Input.is_action_pressed("throw_1") && globals.ball_status == "FIELDER":
 		ball_thrown = true
 
@@ -30,3 +38,16 @@ func _physics_process(delta):
 #		ball.visible = true
 #		ball.apply_impulse(ball_english, throw_target)
 #		ball_thrown = false
+func throw(direction):
+	var temp = global_transform
+	var scene = get_tree().current_scene
+	get_parent().remove_child(self)
+	scene.add_child(self)
+	global_transform = temp
+	velocity = THROW_VELOCITY * Vector2(direction, 1)
+	set_physics_process(true)
+
+func _on_impact(normal : Vector2):
+	velocity = velocity.bounce(normal)
+	velocity *= 0.5 + rand_range(-0.05, 0.05)
+	
